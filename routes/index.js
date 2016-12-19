e329e71bd27b32ea3c62630b7a79ea85bbbdb05b
245160801@qq.com <245160801@qq.com>
2016-12-19 15:22:58 +0800

    edit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	console.log('Time: ', Date.now());
+	next();
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', message: 'Hello, Simon!' });
+});
+
+// 定义 about 页面的路由
+router.get('/about', function(req, res) {
+  res.send('About birds');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('layout', { title: 'Layout' });
+  res.render('users', { title: 'Express', username: 'wangzhe', users: [{ username: 'Wilson' }, { username: 'Wilson Zhong' }, { username: 'Zhong Wei' }] });
 });
 
-router.get('/wangzhe', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('users', { title: 'Express', user: 'wangzhe' });
+  res.render('users', { title: 'Express', username: 'wangzhe', users: [{ username: 'Wilson' }, { username: 'Wilson Zhong' }, { username: 'Zhong Wei' }] });
 });
 
 module.exports = router;

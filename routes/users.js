be58ec838e8fe27e23c3462db91dc18d19e1e7ae
245160801@qq.com <245160801@qq.com>
2017-06-14 10:28:03 +0800

    edit

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('users', { title: 'Express', username: 'wangzhe', users: [{ username: 'Wilson' }, { username: 'Wilson Zhong' }, { username: 'Zhong Wei' }] });
+  res.render('users', { title: 'Express', username: 'wangzhe', users: [{ username: 'Wilson' }, { username: 'Wilson123 Zhong' }, { username: 'Zhong Wei' }] });
 });
 
 router.get('/:id', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('users', { title: 'Express', username: 'wangzhe', users: [{ username: 'Wilson' }, { username: 'Wilson Zhong' }, { username: 'Zhong Wei' }] });
+  res.render('users', { title: 'Express', username: 'wangzhe', users: [{ username: 'Wilson' }, { username: 'Wilson Z2323hong' }, { username: 'Zhong Wei' }] });
 });
 
 module.exports = router;
